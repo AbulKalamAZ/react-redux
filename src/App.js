bf40c2d264e490397a27b54c9063cbeb40be43e3
ak_azad <ak_azad@solsersoft.com>
2019-11-26 19:05:07 +0600

    Styled component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "./App.css";
 
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 3em;
+  font-weight: light;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.div`
+  padding: 2em;
+  margin: 0 auto;
+`;
+
+const Button = styled.button`
+  margin: 1em;
+  padding: 0.25em 1em;
+  font-size: 2.4em;
+  color: ${props => (props.primary ? "white" : "palevioletred")};
+  background: ${props => (props.primary ? "palevioletred" : "transparent")};
+  border: 2px solid palevioletred;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!!</h1>
+      <Wrapper>
+        <Title>Hello, World!</Title>
+      </Wrapper>
+      <Wrapper>
+        <Button>Send Message</Button>
+        <Button primary>Send Message</Button>
+      </Wrapper>
     </div>
   );
 }
