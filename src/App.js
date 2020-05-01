d70db9d1e961a917da59ae5defdd9fdfe67c834b
azad <dev.akazad@hotmail.com>
2020-05-01 23:35:29 +0600

    props and list rendering covered

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
 import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+import User from "./components/user/User";
+
+class App extends React.Component {
+  state = {
+    users: [
+      {
+        name: "Azad",
+        age: "28",
+        skills: ["reading", "writing", "programming"],
+        id: 1,
+      },
+      {
+        name: "Kalam",
+        age: "31",
+        skills: ["reading", "writing", "programming"],
+        id: 2,
+      },
+      {
+        name: "Sabrina",
+        age: "22",
+        skills: ["reading", "writing", "drawing", "Crrafting"],
+        id: 3,
+      },
+      {
+        name: "Alishba",
+        age: "3",
+        skills: ["making fun", "entertaining"],
+        id: 4,
+      },
+    ],
+  };
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div className="App">
+        <User users={users} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/user/User.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./User.scss";
+
+export default function User(props) {
+  const { users } = props;
+
+  const formatSkills = (skills) => {
+    let str = "";
+
+    for (let i = 0; i < skills.length; i++) {
+      if (i === skills.length - 1) {
+        str += `${skills[i]}.`;
+      } else if (i === skills.length - 2) {
+        str += `${skills[i]} and `;
+      } else {
+        str += `${skills[i]}, `;
+      }
+    }
+
+    return str;
+  };
+  return users.map((user) => {
+    return (
+      <div className="user" key={user.id}>
+        <h1>Name: {user.name}</h1>
+        <h3>Age: {user.age}</h3>
+        <h3>Skills: {formatSkills(user.skills)}</h3>
+      </div>
+    );
+  });
+}
